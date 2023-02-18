b96802870146bd9f55f32918d44ee6cd1a58db59
Mateusz <wmateusz993@gmail.com>
2023-02-18 12:18:53 +0100

    Start Project

---
+++ src/main/java/com/myBookSchelf/BookSchelf/BookSchelfApplication.java
@@ -0,0 +1,13 @@
+package com.myBookSchelf.BookSchelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSchelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSchelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myBookSchelf/BookSchelf/BookSchelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myBookSchelf.BookSchelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSchelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
